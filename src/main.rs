6e9ecebe0849bd919a3d2ec2519eee634da891ab
Regis <boudinot.regis@yahoo.com>
2016-03-12 17:23:02 -0700

    just need to serve this file

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
 extern crate iron;
 extern crate rustc_serialize;
+extern crate router;
+extern crate staticfile;
+extern crate mount;
 
 use iron::prelude::*;
 use iron::status;
 use rustc_serialize::json;
+use router::Router;
+use std::path::Path;
 
 mod universe;
 
 fn main() {
-    let trimmed = 1;
+    let mut router = Router::new();
+    router.get("/", handler);
+    router.get("/pub", js);
 
-    let mut universe = vec![];
-    let mut neut = vec![0];
-    let mut prot = vec![0];
-    let mut elec = vec![0];
+    Iron::new(router).http("localhost:3000").unwrap();
 
-    universe::initialize_life(trimmed, &mut universe);
-    universe::particles(&mut universe, &mut neut, &mut prot, &mut elec);
-    universe::charge_of_field(&mut prot, &mut elec, trimmed);
-    universe::atom_charge(&mut universe);
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let trimmed = 1;
 
-    Iron::new(move |_: &mut Request| {
+        let mut universe = vec![];
+        let mut neut = vec![0];
+        let mut prot = vec![0];
+        let mut elec = vec![0];
+
+        universe::initialize_life(trimmed, &mut universe);
+        universe::particles(&mut universe, &mut neut, &mut prot, &mut elec);
+        universe::charge_of_field(&mut prot, &mut elec, trimmed);
+        universe::atom_charge(&mut universe);
         let encoded = json::encode(&universe).unwrap();
 
         Ok(Response::with((status::Ok, "{}", encoded)))
-    }).http("localhost:3000").unwrap();
+    }
+
+    fn js(req: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, Path::new("./staticfile/index.html"))))
+    }
 }
